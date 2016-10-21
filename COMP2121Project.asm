b29e66cf07d6f8eb21212c15e7cbefd0f4d2def8
mattphillips121 <matt.phillips121@hotmail.com>
2016-10-21 14:17:18 +1100

    Added all of the defs and turntable string

--- COMP2121Project.asm
+++ COMP2121Project.asm
@@ -1,6 +1,26 @@
 .include "m2560def.inc"
 
+.def status = r29   ; 000 D MM PP 
+;D = Direction of rotation (0 clockwise 1 counterclockwise) 
+;P = Power (1-3)  
+;M = Menu 
+;Menu:
+;* 00 running
+;* 01 Entry
+;* 10 Paused
+;* 11 Finished
+.def minutes = r27
+.def seconds = r28
+.def LCDtime = r26
+
+.dseg
+Turntable:
+	.db "-/|``|/-"
+
+.cseg
+
 Reset:
+	
 
 halt:
 	rjmp halt
