f938850c9c854b87b50b34c6ce812879cd9a6f9d
mattphillips121 <matt.phillips121@hotmail.com>
2016-10-21 14:35:03 +1100

    Initialised ports for push buttons, lcd and timer0 and SP

--- COMP2121Project.asm
+++ COMP2121Project.asm
@@ -20,7 +20,37 @@ Turntable:
 .cseg
 
 Reset:
+
+	; Initialise stack for interrupts
+	ldi r16, high(RAMEND)
+	out SPH, r16
+	ldi r16, low(RAMEND)
+	out SPL, r16
 	
+	; Initialise timer 0 with prescalar 8
+	; Interrupt on overflow
+	clr r16
+	out TCCR0A, r16
+	ldi r16, 0b00000010
+	out TCCR0B, r16
+	ldi r16, 1<<TOIE0 	
+	sts TIMSK0, r16 ; Interrupt on overflow
+	
+	; Initialise the LCD Ports
+	ser r16
+	out DDRF, r16
+	out DDRA, r16
+	clr r16
+	out PORTF, r16
+	out PORTA, r16
+
+	; Initialise the Push buttons
+	clr r16
+	out DDRD, r16 ; Input
+	ser r16
+	out PORTD, r16 ; Pull up resistors
+
+	sei
 
 halt:
 	rjmp halt
